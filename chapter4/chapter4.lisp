89fd5e0d0f28a43b285e40945fd711be9012d3cf
Kei Murakami <buncho514@icloud.com>
2021-04-03 22:33:50 +0900

    add my-length

--- chapter4/chapter4.lisp
+++ chapter4/chapter4.lisp
@@ -1,2 +1,15 @@
-; SLIME 2.26.1
-CL-USER> 
+(if '()
+    'i-am-true
+    'i-am-false)
+
+(if '(1)
+    'i-am-true
+    'i-am-false)
+
+(defun my-length(list)
+  (if list
+      (1+ (my-length (cdr list)))
+      0))
+
+(my-length '(list with four symbols))
+
