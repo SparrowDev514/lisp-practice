c547f1a4d8a8930cd98902add4d91f4f8be9553a
Kei Murakami <buncho514@icloud.com>
2021-04-05 12:06:03 +0900

    第四章:

--- chapter4/chapter4.lisp
+++ chapter4/chapter4.lisp
@@ -1,15 +1,5 @@
 (if '()
-    'i-am-true
-    'i-am-false)
-
-(if '(1)
-    'i-am-true
-    'i-am-false)
-
-(defun my-length(list)
-  (if list
-      (1+ (my-length (cdr list)))
-      0))
-
-(my-length '(list with four symbols))
+    (princ "i-am-true")
+    (princ "i-am-false")
+    )
 
